f87392d05c682d47c71f3c2de2b9f700b13d5134
Fausto Neves Kina <fausto.kina@cromai.com>
2021-07-26 17:32:03 -0300

    Register screen and its navigation added, buttons now have the same gradient color as background

---
+++ app/src/main/java/br/com/fausto/headsuppics/ui/fragment/CreateAccountFragment.kt
@@ -0,0 +1,23 @@
+package br.com.fausto.headsuppics.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.fausto.headsuppics.R
+
+class CreateAccountFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_create_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/br/com/fausto/headsuppics/ui/fragment/LoginFragment.kt
+++ app/src/main/java/br/com/fausto/headsuppics/ui/fragment/LoginFragment.kt
@@ -4,15 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import br.com.fausto.headsuppics.R
 
 class LoginFragment : Fragment() {
+
+    private lateinit var createAccountText: TextView
+    private lateinit var loginButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        createAccountText = requireView().findViewById(R.id.createAccountText)
+        loginButton = requireView().findViewById(R.id.loginButton)
+
+        createAccountText.setOnClickListener {
+            findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToCreateAccountFragment()
+            )
+        }
+
+        loginButton.setOnClickListener {
+            findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToPictureFragment()
+            )
+        }
     }
 }
